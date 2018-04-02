6b21872190926fe7cbb6668ea7e499ce2c09b0ce
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-04-02 09:51:06 +0000

    Basics with JSX, constructor, state and events

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'App title'
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      title: "New app title"
+    })
+  }
+
   render() {
+    var title = "This is Zulh's app"
+
+    /* ----------------------------------------------------
+    * to iterate items list (must be in render() function)
+    * ------------------------------------------------------
+    * var items = ["Item 1", "Item 2", "Item 3"]
+    *  {
+    *    items.map(item => {
+    *      return (
+    *        <p key={item} onClick={this.onClick}>{item}</p>
+    *      )
+    *    })
+    *  }
+    */
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{title}</h1>
+        <h2>{this.state.title}</h2>
+        <div onClick={this.onClick}>Click here</div>
       </div>
     );
   }
