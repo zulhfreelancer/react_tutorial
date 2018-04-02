599cabaa1be83cc69a4dd6c37e8d049f73f56af3
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-04-02 10:00:33 +0000

    Refactor component - reducing 'this.props' calls

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 class MyComponent extends Component {
-
   render() {
+    var {title, name} = this.props;
+
     return (
       <div className="component">
         <h1>This is a component</h1>
-        <h2>Title: {this.props.title}</h2>
-        <h2>Name: {this.props.name}</h2>
+        <h2>Title: {title}</h2>
+        <h2>Name: {name}</h2>
       </div>
     );
   }
-
 }
 
 export default MyComponent;
